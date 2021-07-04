c1fa2c4d8e2f39f08db9b1ed56ddd6abbca44a80
Lukas Lanzner <lanzner09@gmail.com>
2021-07-04 20:27:01 +0200

    added some new Command Types & improved some RequestParameterTypes

--- src/main/java/team/dotspace/squidly/requests/command/RequestParameterType.java
+++ src/main/java/team/dotspace/squidly/requests/command/RequestParameterType.java
@@ -13,11 +13,12 @@ public enum RequestParameterType {
   SESSION("session"),
   LANGUAGE_CODE("languageCode"),
 
-  PLAYER("player"),
+  PLAYER("player"), //PlayerName or playerId
   PLAYER_ID("playerId"),
+  PLAYER_ID_SET("playerId"),
   PLAYER_NAME("playerName"),
   GAMER_TAG("gamerTag"),
-  SEARCH_PLAYER("searchPlayer"),
+  SEARCH_PLAYER("searchPlayer"), //name or gamer_tag of player for whom to search
 
   CHAMPION_ID("championId"),
   GOD_ID("godId"),
@@ -25,13 +26,13 @@ public enum RequestParameterType {
   QUEUE("queue"),
   SEASON("season"),
   TIER("tier"),
-  MATCH_ID("match_id"),
+  MATCH_ID("match_id"), //The id of the match. Can be obtained from /getmatchHistory, /gettopmatches & /getmatchidsbyqueue
 
-  PORTAL_ID("portalId"),
-  PORTAL_USER_ID("portalUserId"),
+  PORTAL_ID("portalId"), //1:Hi-Rez, 5:Steam, 9:PS4, 10:Xbox, 22:Switch, 25:Discord, 28:Epic
+  PORTAL_USER_ID("portalUserId"), //The (usually) 3rd-Party identifier for a Portal.  Examples:  Steam ID, PS4 GamerTag, Xbox GamerTag, Switch GamerTag.
 
-  CLAN_ID("clanid"),
-  SEARCH_TEAM("searchTeam");
+  CLAN_ID("clanid"), //id of the clan. Can be obtained from searchteams
+  SEARCH_TEAM("searchTeam"); //name of clan for whom to search
 
   String s;
   RequestParameterType(String s) {
