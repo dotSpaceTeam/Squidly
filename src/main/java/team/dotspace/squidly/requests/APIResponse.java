7054d8b7e4451efd5bd580be7399072999a75cbd
Lukas Lanzner <lanzner09@gmail.com>
2021-07-18 20:07:25 +0200

    update APIResponse#isFailure

--- src/main/java/team/dotspace/squidly/requests/APIResponse.java
+++ src/main/java/team/dotspace/squidly/requests/APIResponse.java
@@ -28,10 +28,10 @@ public record APIResponse(int statusCode, String statusText, JsonNode jsonNode) 
     if (this.statusCode != 200)
       return true;
 
-    if (this.jsonNode.isArray() && this.jsonNode.getArray() == null || this.jsonNode.getArray().length() < 0)
+    if (this.jsonNode.isArray() && (this.jsonNode.getArray() == null || this.jsonNode.getArray().length() < 0))
       return true;
 
-    if (!this.jsonNode.isArray() && this.jsonNode.getObject() == null || this.jsonNode.getObject().getString("ret_msg").equals("Approved"))
+    if (!this.jsonNode.isArray() && (this.jsonNode.getObject() == null || this.jsonNode.getObject().getString("ret_msg").equals("Approved")))
       return true;
 
     return false;
